dbba2366cb708ccfa6512d55636e50b36e794635
Vansh <vanshdoc7@gmail.com>
2025-10-09 16:12:35 +0530

    first commit

---
+++ back/config/db.js
@@ -0,0 +1,19 @@
+// for mongoDB connection try...catch → catches errors if connection fails.
+// process.exit(1) → stops program if it can’t connect.
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ back/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+connectDB(); //connect mongoDB
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
